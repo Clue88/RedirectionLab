e8c5c1d03d7a430c869c8710f3f0f6550d397afc
Christopher Liu <christopherliu@sorcero.com>
2020-12-23 13:48:17 -0500

    Write MakeStars

--- MakeStars.java
+++ MakeStars.java
@@ -1,4 +1,19 @@
 import java.util.*;
 public class MakeStars {
-    
+    public static void main(String[] args) {
+        Scanner n = new Scanner(System.in);
+        while (n.hasNextLine()) {
+            Scanner line = new Scanner(n.nextLine());
+            while (line.hasNext()) {
+                String stars = "";
+                int length = line.next().length();
+                for (int i = 0; i < length; i++) {
+                    stars += "*";
+                }
+                System.out.print(stars + " ");
+            }
+            System.out.println();
+        }
+        n.close();
+    }
 }
